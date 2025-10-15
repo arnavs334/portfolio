8d6cb6ec5726cc186ac570a5b31fad735a75fca2
arnavs334 <singh.arnavs443@gmail.com>
2025-10-14 22:47:37 -0400

    Configure for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/portfolio/", // This will be your repository name
   server: {
     host: "::",
     port: 8080,
